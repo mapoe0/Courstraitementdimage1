3202848d109f5dfd62543cb77171a53bfd43bf78
Ysoline SCHOONAERT <ysoline.schoonaert@student.junia.com>
2022-01-07 16:45:03 +0100

    Ajoutez des fichiers projet.

---
+++ Courstraitementdimage1/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace Courstraitementdimage1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbSource = new System.Windows.Forms.PictureBox();
+            this.BtnNoirEtBlanc = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnBin = new System.Windows.Forms.Button();
+            this.tbSeuil = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.negBtn = new System.Windows.Forms.Button();
+            this.histogrameBtn = new System.Windows.Forms.Button();
+            this.recadrage = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSeuil)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbSource
+            // 
+            this.pbSource.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbSource.Location = new System.Drawing.Point(190, 68);
+            this.pbSource.Name = "pbSource";
+            this.pbSource.Size = new System.Drawing.Size(256, 256);
+            this.pbSource.TabIndex = 0;
+            this.pbSource.TabStop = false;
+            // 
+            // BtnNoirEtBlanc
+            // 
+            this.BtnNoirEtBlanc.Location = new System.Drawing.Point(482, 26);
+            this.BtnNoirEtBlanc.Name = "BtnNoirEtBlanc";
+            this.BtnNoirEtBlanc.Size = new System.Drawing.Size(256, 23);
+            this.BtnNoirEtBlanc.TabIndex = 1;
+            this.BtnNoirEtBlanc.Text = "Noir et Blanc";
+            this.BtnNoirEtBlanc.UseVisualStyleBackColor = true;
+            this.BtnNoirEtBlanc.Click += new System.EventHandler(this.BtnNoirEtBlanc_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(482, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(856, 67);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnBin
+            // 
+            this.btnBin.Location = new System.Drawing.Point(856, 26);
+            this.btnBin.Name = "btnBin";
+            this.btnBin.Size = new System.Drawing.Size(256, 23);
+            this.btnBin.TabIndex = 4;
+            this.btnBin.Text = "binariser";
+            this.btnBin.UseVisualStyleBackColor = true;
+            this.btnBin.Click += new System.EventHandler(this.btnBin_Click);
+            // 
+            // tbSeuil
+            // 
+            this.tbSeuil.Location = new System.Drawing.Point(856, 365);
+            this.tbSeuil.Maximum = 255;
+            this.tbSeuil.Name = "tbSeuil";
+            this.tbSeuil.Size = new System.Drawing.Size(256, 45);
+            this.tbSeuil.TabIndex = 5;
+            this.tbSeuil.Scroll += new System.EventHandler(this.tbSeuil_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(965, 406);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "seuil: 127";
+            // 
+            // negBtn
+            // 
+            this.negBtn.Location = new System.Drawing.Point(482, 352);
+            this.negBtn.Name = "negBtn";
+            this.negBtn.Size = new System.Drawing.Size(256, 23);
+            this.negBtn.TabIndex = 7;
+            this.negBtn.Text = "Negative";
+            this.negBtn.UseVisualStyleBackColor = true;
+            this.negBtn.Click += new System.EventHandler(this.negBtn_Click);
+            // 
+            // histogrameBtn
+            // 
+            this.histogrameBtn.Location = new System.Drawing.Point(482, 401);
+            this.histogrameBtn.Name = "histogrameBtn";
+            this.histogrameBtn.Size = new System.Drawing.Size(256, 23);
+            this.histogrameBtn.TabIndex = 8;
+            this.histogrameBtn.Text = "Histograme";
+            this.histogrameBtn.UseVisualStyleBackColor = true;
+            this.histogrameBtn.Click += new System.EventHandler(this.histogrameBtn_Click);
+            // 
+            // recadrage
+            // 
+            this.recadrage.Location = new System.Drawing.Point(1175, 352);
+            this.recadrage.Name = "recadrage";
+            this.recadrage.Size = new System.Drawing.Size(256, 23);
+            this.recadrage.TabIndex = 9;
+            this.recadrage.Text = "recadrage";
+            this.recadrage.UseVisualStyleBackColor = true;
+            this.recadrage.Click += new System.EventHandler(this.recadrage_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox3.Location = new System.Drawing.Point(1175, 68);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1494, 527);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.recadrage);
+            this.Controls.Add(this.histogrameBtn);
+            this.Controls.Add(this.negBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbSeuil);
+            this.Controls.Add(this.btnBin);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BtnNoirEtBlanc);
+            this.Controls.Add(this.pbSource);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSeuil)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbSource;
+        private System.Windows.Forms.Button BtnNoirEtBlanc;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnBin;
+        private System.Windows.Forms.TrackBar tbSeuil;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button negBtn;
+        private System.Windows.Forms.Button histogrameBtn;
+        private System.Windows.Forms.Button recadrage;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Courstraitementdimage1/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Courstraitementdimage1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap imagesrc;
+        Bitmap imageDestination;
+        Bitmap imageDestination2;
+        int seuil = 127;
+        int[] greyCount;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            imagesrc = new Bitmap(@"C:\HEI4IMS\lena.jpg");
+            pbSource.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pbSource.Image = imagesrc;
+
+        }
+
+        private void BtnNoirEtBlanc_Click(object sender, EventArgs e)
+        {
+            imageDestination = new Bitmap(pbSource.Image.Width, pbSource.Image.Height);
+
+            for (int x = 0; x < pbSource.Image.Width; x++)
+            {
+                for (int y = 0; y < pbSource.Image.Height; y++)
+                {
+                    Color pixelColor = imagesrc.GetPixel(x, y);
+
+                    int grey = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+
+                    imageDestination.SetPixel(x, y, Color.FromArgb(grey, grey, grey));
+
+                }
+            }
+            pictureBox1.Image = imageDestination;
+        }
+
+        private void btnBin_Click(object sender, EventArgs e)
+        {
+            imageDestination2 = new Bitmap(pbSource.Image.Width, pbSource.Image.Height);
+            for (int x = 0; x < pbSource.Image.Width; x++)
+            {
+                for (int y = 0; y < pbSource.Image.Height; y++)
+                {
+                    Color pixelColor = imageDestination.GetPixel(x, y);
+                    Color newColor;
+                    if (pixelColor.R >= seuil)
+                    {
+                        newColor = Color.FromArgb(255, 255, 255);
+                    }
+                    else
+                    {
+                        newColor = Color.FromArgb(0, 0, 0);
+                    }
+                    imageDestination2.SetPixel(x, y, newColor);
+
+                }
+            }
+            pictureBox2.Image = imageDestination2;
+        }
+
+        private void tbSeuil_Scroll(object sender, EventArgs e)
+        {
+            seuil = tbSeuil.Value;
+            label1.Text = "seuil: " + seuil.ToString();
+            btnBin.PerformClick();
+
+        }
+
+        private void negBtn_Click(object sender, EventArgs e)
+        {
+            imageDestination = new Bitmap(pbSource.Image.Width, pbSource.Image.Height);
+
+            for (int x = 0; x < pbSource.Image.Width; x++)
+            {
+                for (int y = 0; y < pbSource.Image.Height; y++)
+                {
+                    Color pixelColor = imagesrc.GetPixel(x, y);
+
+                    int r = 255 - pixelColor.R;
+                    int b = 255 - pixelColor.B;
+                    int g = 255 - pixelColor.G;
+
+                    imageDestination.SetPixel(x, y, Color.FromArgb(r, g, b));
+
+                }
+            }
+            pictureBox1.Image = imageDestination;
+        }
+
+        private void histogrameBtn_Click(object sender, EventArgs e)
+        {
+
+            greyCount = new int[256];
+            for (int i = 0; i < greyCount.Length; i++)
+            {
+                greyCount[i] = 0;
+            }
+            greyCount = Histogramme(imageDestination, greyCount);
+            DrawHistogram(greyCount,0, greyCount.Length);
+
+        }
+        private int[] Histogramme(Bitmap bitmap, int[] greyCount)
+        {
+            for (int x = 0; x < pbSource.Image.Width; x++)
+            {
+                for (int y = 0; y < pbSource.Image.Height; y++)
+                {
+                    greyCount[bitmap.GetPixel(x, y).R] += 1;                 
+                }
+            }
+            int cpt = 0;
+            int max = greyCount.Max();
+            Console.WriteLine(max.ToString());
+            for(int i = 0; i < greyCount.Length; i++)
+            {
+                greyCount[i] = (256 * greyCount[i]) / max;
+                
+                
+            }
+            Console.WriteLine(cpt.ToString());
+            // mise à l'échelle: plus grande valeur = 256
+
+            return greyCount;
+        }
+        public void DrawHistogram(int[] greyCount, int seuilBas, int seuilHaut)
+        {
+            
+            // int x1 y1;
+            int y1, y2;            
+            using (Graphics g = pictureBox2.CreateGraphics())
+            {
+                for (int i = seuilBas; i < seuilHaut; i++)
+                {
+                    
+                    y1 = 255;
+                    y2 = 255 - greyCount[i];
+                    g.DrawLine(Pens.Black, new Point(i, y1), new Point(i, y2));
+                }
+                
+            }
+            
+        }
+
+        private void recadrage_Click(object sender, EventArgs e)
+        {
+            int seuilBas = 0;
+            int seuilHaut = 0;
+            // trouver les seuils
+            for(int i = 0; i < greyCount.Length; i++)
+            {
+                //seuil bas
+                if(greyCount[i] != 0 && seuilBas ==0)
+                {
+                    seuilBas = i;
+                }
+
+                // seuil haut
+                if(greyCount[i]==0 && seuilHaut ==0 && seuilBas != 0)
+                {
+                    seuilHaut = i;
+                }
+            }
+            
+            
+            Bitmap imageDestinationRecadre = new Bitmap(pbSource.Image.Width, pbSource.Image.Height);
+            for (int x = 0; x < pbSource.Image.Width; x++)
+            {
+                for (int y = 0; y < pbSource.Image.Height; y++)
+                {
+                    Color pix = imageDestination.GetPixel(x, y);
+                    int p = pix.R;
+                    p = (p-seuilBas) / 256;
+                    imageDestinationRecadre.SetPixel(x, y, Color.FromArgb(p, p, p));
+                }
+            }
+            pictureBox3.Image = imageDestinationRecadre;
+        }
+    }
+}
+
+
+
